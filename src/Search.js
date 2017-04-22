1d2f10a90d256b4e43ae71eb4c7a8cf32e6a4037
aurimas-darguzis <aurimas@darguzis.com>
2017-04-22 17:00:22 +0100

    showing json on UI

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import preload from '../public/data.json'
 
 class Search extends React.Component {
   render () {
     return (
-      <h1>Search Page</h1>
+      <div>
+        <pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
+      </div>
     )
   }
 }
 
 export default Search
+
+// JSON.stringify parameters:
+// 1st - the data you want to stringify
+// 2nd - replacer function, actually goes into json and replace data. Usually you always put null as 2nd parameter
+// 3rd - the desired indentation 
