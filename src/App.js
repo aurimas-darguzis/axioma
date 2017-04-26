3bfd9daa79fe7aa0d2bd23e29761688f985a3ec4
aurimas-darguzis <aurimas@darguzis.com>
2017-04-26 09:32:55 +0100

    passing shows and props from App to Search through router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-
 import './App.css'
 import Landing from './Landing'
 import Search from './Search'
 import Details from './Details'
+import preload from '../public/data'
 
 class App extends React.Component {
   render () {
@@ -16,10 +16,15 @@ class App extends React.Component {
             <li><Link to='/search'>Search</Link></li>
           </ul>
           <Route exact path='/' component={Landing} />
-          <Route path='/search' component={Search} />
+          <Route 
+            path='/search' 
+            component={(props) => <Search shows={preload.shows} {...props} />}
+            
+             />
           <Route path='/details/:id' component={Details} />
         </div>
       </Router>
+      
     )
   }
 }
