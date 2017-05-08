e0a66995c7b4ffce97109ab927f4fb06d8f5fb47
aurimas-darguzis <aurimas@darguzis.com>
2017-05-08 20:21:51 +0100

    starting from a different angle a bit

--- src/App.js
+++ src/App.js
@@ -11,28 +11,7 @@ import preload from '../public/data'
 class App extends React.Component {
   render () {
     return (
-      <Provider store={store}>
-        <div>
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/search'>Search</Link></li>
-          </ul>
-          <Route exact path='/' component={Landing} />
-          <Route
-            path='/search'
-            component={(props) => <Search shows={preload.shows} {...props} />}
-
-            />
-          <Route
-            path='/details/:id'
-            component={(props) => {
-              const shows = preload.shows.filter((show) => props.params.id === show.imdbID)
-              return <Details show={shows[0]} {...props} />
-            }
-              }
-            />
-        </div>
-      </Provider>
+      <h1>Hey</h1>
     )
   }
 }
--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 import { connect } from 'react-redux'
 import { getOMDBDetails } from './actionCreators'
 import Header from './Header'
